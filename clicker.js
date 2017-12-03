d3b71af13bd84a86a71679b370b903a796050d79
marleigh <volcana@gmail.com>
2017-12-02 19:13:26 -0800

    Random update

--- clicker.js
+++ clicker.js
@@ -10,3 +10,13 @@ function increment() {
 }
 
 //auto produce cats  -- something like 'your cats are now breeding'
+//change this to a switch later?
+
+if (catClick == 20 ) {
+  //display a button to start auto producing cats
+}
+
+//run a function that is connected to the button
+//when that button is clicked, start auto producing cats
+
+//needs to disappear after clicking so the function can't run again until we want it to 
